b36c22b94a623c8d83a8b32b0cfc45bb0971b4a7
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-24 07:34:19 +0100

    add function for getting position of the head

--- src/snake.rs
+++ src/snake.rs
@@ -50,4 +50,9 @@ impl Snake {
             draw_block(SNAKE_COLOR, block.x, block.y, con, g);
         }
     }
+
+    pub fn head_position(&self) -> (i32, i32) {
+        let head_block = self.body.front().unwrap();
+        (head_block.x, head_block.y)
+    }
 }
