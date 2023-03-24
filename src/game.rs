3285f448d1fcbd6962b37f6908b74f6afc9a7e09
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-24 08:19:18 +0100

    add constants for food color, boerder color, game over color, moving period (fps) and time to wait in seconds before restarting the game

--- src/game.rs
+++ src/game.rs
@@ -5,3 +5,11 @@ use rand::{thread_rng, Rng};
 
 use crate::snake::{Direction, Snake};
 use crate::draw::{draw_block, draw_rectangle};
+
+const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
+const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
+const GAME_OVER_COLOR: Color = [0,90, 0.00, 0.00, 0.5];
+
+/// Frames per Second
+const MOVING_PERIOD: f64 = 0.1;
+const RESTART_TIME: f64 = 1.0;
