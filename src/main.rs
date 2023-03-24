80736a46bb974692d84da82124837920eb5c1fe1
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-24 09:21:39 +0100

    add constant for background color

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use piston_window::types::Color;
 use crate::game::Game;
 use crate::draw::to_coord_u32;
 
+const BACKGROUND_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
 fn main() {
     println!("Hello, world!");
 }
