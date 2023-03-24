491b7a9d56b3c14511c5a57fb4ca03d855f1d127
Philipp Pickartz <pickartzphilipp@googlemail.com>
2023-03-24 09:37:25 +0100

    formatted code with rustfmt

--- src/draw.rs
+++ src/draw.rs
@@ -1,5 +1,5 @@
-use piston_window::{rectangle, Context, G2d};
 use piston_window::types::Color;
+use piston_window::{rectangle, Context, G2d};
 
 const BLOCK_SIZE: f64 = 25.0;
 
@@ -19,18 +19,31 @@ pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
         color,
         [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
         con.transform,
-        g
+        g,
     );
 }
 
-pub fn draw_rectangle(color: Color, x: i32, y: i32, width: i32, height: i32, con: &Context, g: &mut G2d) {
+pub fn draw_rectangle(
+    color: Color,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+    con: &Context,
+    g: &mut G2d,
+) {
     let x = to_coord(x);
     let y = to_coord(y);
 
     rectangle(
         color,
-        [x, y, BLOCK_SIZE * (width as f64), BLOCK_SIZE * (height as f64)],
+        [
+            x,
+            y,
+            BLOCK_SIZE * (width as f64),
+            BLOCK_SIZE * (height as f64),
+        ],
         con.transform,
-        g
+        g,
     )
 }
--- src/snake.rs
+++ src/snake.rs
@@ -1,6 +1,6 @@
-use std::collections::LinkedList;
-use piston_window::{Context, G2d};
 use piston_window::types::Color;
+use piston_window::{Context, G2d};
+use std::collections::LinkedList;
 
 use crate::draw::draw_block;
 
@@ -11,7 +11,7 @@ pub enum Direction {
     Up,
     Down,
     Left,
-    Right
+    Right,
 }
 
 impl Direction {
@@ -20,7 +20,7 @@ impl Direction {
             Direction::Up => Direction::Down,
             Direction::Down => Direction::Up,
             Direction::Left => Direction::Right,
-            Direction::Right => Direction::Left
+            Direction::Right => Direction::Left,
         }
     }
 }
@@ -28,23 +28,27 @@ impl Direction {
 #[derive(Clone, Debug)]
 struct Block {
     x: i32,
-    y: i32
+    y: i32,
 }
 
 pub struct Snake {
     direction: Direction,
     body: LinkedList<Block>,
-    tail: Option<Block>
+    tail: Option<Block>,
 }
 
 impl Snake {
-    pub fn new(x: i32, y:i32) -> Snake {
+    pub fn new(x: i32, y: i32) -> Snake {
         let mut body: LinkedList<Block> = LinkedList::new();
         body.push_back(Block { x: x + 2, y: y });
         body.push_back(Block { x: x + 1, y: y });
         body.push_back(Block { x: x, y: y });
 
-        Snake { direction: Direction::Right, body: body, tail: None }
+        Snake {
+            direction: Direction::Right,
+            body: body,
+            tail: None,
+        }
     }
 
     pub fn draw(&self, con: &Context, g: &mut G2d) {
@@ -61,16 +65,28 @@ impl Snake {
     pub fn move_forward(&mut self, dir: Option<Direction>) {
         match dir {
             Some(d) => self.direction = d,
-            None => ()
+            None => (),
         }
 
         let (last_x, last_y): (i32, i32) = self.head_position();
 
         let new_block = match self.direction {
-            Direction::Up => Block {x: last_x, y: last_y - 1},
-            Direction::Down => Block {x: last_x, y: last_y + 1},
-            Direction::Left => Block { x: last_x - 1, y: last_y },
-            Direction::Right => Block { x: last_x + 1, y: last_y }
+            Direction::Up => Block {
+                x: last_x,
+                y: last_y - 1,
+            },
+            Direction::Down => Block {
+                x: last_x,
+                y: last_y + 1,
+            },
+            Direction::Left => Block {
+                x: last_x - 1,
+                y: last_y,
+            },
+            Direction::Right => Block {
+                x: last_x + 1,
+                y: last_y,
+            },
         };
 
         self.body.push_front(new_block);
@@ -95,7 +111,7 @@ impl Snake {
             Direction::Up => (head_x, head_y - 1),
             Direction::Down => (head_x, head_y + 1),
             Direction::Left => (head_x - 1, head_y),
-            Direction::Right => (head_x + 1, head_y)
+            Direction::Right => (head_x + 1, head_y),
         }
     }
 
